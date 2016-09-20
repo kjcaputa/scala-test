5b7d51d5ede59dac5ad02d287f45e779c1ca9d63
Kris <kjcaputa@gmail.com>
2016-09-20 08:04:57 +0100

    feature-01-01: Add product prices. Test for product prices are passed

--- src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
+++ src/main/scala/net/atos/kjc/fruitshop/checkoutsystem/Checkout.scala
@@ -1,7 +1,7 @@
 package net.atos.kjc.fruitshop.checkoutsystem
 
 object Checkout {
-  val prices: Map[String,Double] = ???
+  val prices: Map[String,Double] = Map("apple" -> 0.60, "orange" -> 0.25)
 
   def processor(listOfItems: List[String]): String =  ???
 }
